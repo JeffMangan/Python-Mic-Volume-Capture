4c9a6af385b4fff269dcb2ffca4310792d878328
Jeff Mangan <jeff@magentatech.com>
2022-10-08 20:10:24 -0600

    cleaned up code
    cleaned up code and repeated logic

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 # Print out realtime audio volume as ascii bars
-import sys
-import traceback
-
 import numpy as np
 import sounddevice as sd
-
 from colored import fg, bg, attr
+import time as t
 
-
-duration: int = 100
-i = 0
+# does not seem to be accurate, but too low will cause code to crash after a while
+pause_between_lines = 5 * 10000000000000
 
 # https://pypi.org/project/colored/
+#################### choices of colors to use ####################
 green = 2
 blue = 4
 yellow = 226
@@ -19,79 +16,54 @@ purple = 54
 magenta = 5
 orange = 208
 red = 9
-dark_red = 1
-
+dark_red = 124
+#################### actual line colors chosen ####################
 low = blue
 medium = green
 high = yellow
-extreme = orange
-max = red
-
-
-
-
-
-lowVolume = 10
+higher = orange
+highest = red
+#################### multiplier for line to extend by x number of chars ####################
+line_length_multiplier = 10
+#################### number of lines to print for highest to make it more noticeable ####################
+highest_number_lines = 100
+#################### volume max for each category ####################
+lowVolume = 15
 mediumVolume = lowVolume*2
-warningVolume = lowVolume*3
-highVolume = lowVolume*4
-maxVolume = lowVolume*5
+high_volume = lowVolume * 3
+higher_volume = lowVolume * 4
+highest_volume = lowVolume * 5
 
-x = '%s%s' + str(int(20)) + str("=" * (int(20 * 2.5))) + '%s'
-# print(x % (fg(low), bg(low), attr(0)))
-# print(x % (fg(medium), bg(medium), attr(0)))
-# print(x % (fg(warning), bg(warning), attr(0)))
-# print(x % (fg(high), bg(high), attr(0)))
-# print(x % (fg(max), bg(max), attr(0)))
-# sys.exit(0)
-
+# pints the lines for each volume measurement
+def print_line(txt, color, attr, num_of_rows, pause_in_seconds):
+    i = 1
+    while i <= num_of_rows:
+        print(txt % (fg(color), bg(color), attr))
+        i += 1
+    t.sleep(pause_in_seconds)
+    return
 
 def audio_callback(indata, frames, time, status):
-    global i
-    while i < 100:
-        i = i + 1
-        return
-    currentVolume = np.linalg.norm(indata) * 10
-    # print(str(int(volume_norm)))
-    display_length = currentVolume
-    if currentVolume > maxVolume:
-        display_length = highVolume*1.2
+    current_volume = np.linalg.norm(indata) * line_length_multiplier
+    if current_volume < 1:
+        current_volume = 1
+    if current_volume >= highest_volume:
+        current_volume = highest_volume
 
-    x = '%s%s' + str(int(display_length)) + str("=" * (int(display_length*2.5))) + '%s'
-    if currentVolume > maxVolume:
-        print(x % (fg(max), bg(max), attr(0)))
-        print(x % (fg(max), bg(max), attr(0)))
-        print(x % (fg(max), bg(max), attr(0)))
-        print(x % (fg(max), bg(max), attr(0)))
-        print(x % (fg(max), bg(max), attr(0)))
-    elif currentVolume < lowVolume:
-        # x = '%s%s' + str("=" * int(volume_norm)) + '%s'
-        # print(low)
-        print(x % (fg(low), bg(low), attr(0)))
-        print(x % (fg(low), bg(low), attr(0)))
-    # elif volume_norm < 51:
-        # x = '%s%s' + str("=" * int(volume_norm)) + '%s'
-        # print(x % (fg(yellow), bg(yellow), attr(0)))
-    elif currentVolume < mediumVolume:
-        # x = '%s%s' + str("=" * int(volume_norm)) + '%s'
-        # print(medium)
-        print(x % (fg(medium), bg(medium), attr(0)))
-        print(x % (fg(medium), bg(medium), attr(0)))
-    # elif volume_norm < 200:
-        # print(Colors.PINK + str(int(volume_norm)), "_", "=" * int(volume_norm) + Colors.PINK)
-        # print(x % (fg(magenta), bg(magenta), attr(0)))
-    elif currentVolume < warningVolume:
-        # x = '%s%s' + str("=" * int(volume_norm)) + '%s'
-        # print(warning)
-        print(x % (fg(high), bg(high), attr(0)))
-        print(x % (fg(high), bg(high), attr(0)))
+    # line_text = '%s%s' + str(int(current_volume)) + str("=" * (int(current_volume*2.5))) + '%s'
+    line_text = '%s%s' + str("_" * (int(current_volume*2.5))) + '%s'
+
+    if current_volume >= highest_volume:
+        print_line(line_text, highest, attr(0), highest_number_lines, 2)
+    elif current_volume < lowVolume:
+        print_line(line_text, low, attr(0), 2, 0)
+    elif current_volume < mediumVolume:
+        print_line(line_text, medium, attr(0), 2, 0)
+    elif current_volume < high_volume:
+        print_line(line_text, high, attr(0), 2, 0)
     else:
-        # x = '%s%s' + str("=" * int(volume_norm)) + '%s'
-        # print(high)
-        print(x % (fg(extreme), bg(extreme), attr(0)))
-        print(x % (fg(extreme), bg(extreme), attr(0)))
-
+        print_line(line_text, higher, attr(0), 2, 0)
 
 stream = sd.InputStream(callback=audio_callback)
 with stream:
-    sd.sleep(duration * 100000000)
+   sd.sleep(pause_between_lines) # * 100000000)
